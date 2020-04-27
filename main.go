b686ca2cf7c92869bb7e238e7d3e37acbdd5dca4
THEToilet <bp18015@shibaura-it.ac.jp>
2020-04-27 22:30:15 +0900

    :tada: First Commit

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+func main() {
+	// db接続
+	db, err := sqlConnect()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	
+	error := db.Create(&Users{
+								name: "Toilet",
+								accepted_count: 123,
+								accepted_count_rank: 12,
+								rated_point_sum: 34,
+								rated_point_sum_rank: 12,
+								created_time: getDate(),
+								updated_time: getDate(),
+	}).Error
+	
+  if error != nil {
+        fmt.Println(error)
+    } else {
+        fmt.Println("データ追加成功")
+   }
+
+	//データを格納する変数を定義
+	users := []Users{}
+
+	//全取得
+	db.Find(&users)
+
+	//表示
+	for _, user := range users {
+		fmt.Println(user.id)
+		fmt.Println(user.name)
+		fmt.Println(user.accepted_count)
+		fmt.Println(user.accepted_count_rank)
+		fmt.Println(user.rated_point_sum)
+		fmt.Println(user.rated_point_sum_rank)
+		fmt.Println(user.created_time)
+		fmt.Println(user.updated_time)
+	}
+
+}
+
+
+func getDate() string {
+    const layout = "2006-01-02 15:04:05"
+    now := time.Now()
+    return now.Format(layout)
+}
+
+// SQLConnect DB接続
+func sqlConnect() (database *gorm.DB, err error) {
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "pass"
+	PROTOCOL := "tcp(127.0.0.1:3306)"
+	DBNAME := "user_db"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	return gorm.Open(DBMS, CONNECT)
+}
+
+// Users ユーザー情報のテーブル情報
+type Users struct {
+	id                   int
+	name                 string    `json:"name"`
+	accepted_count       int       `json:"accepted_count"`
+	accepted_count_rank  int       `json:"accepted_count_rank"`
+	rated_point_sum      int       `json:"rated_point_sum"`
+	rated_point_sum_rank int       `json:"rated_point_sum_rank"`
+	created_time         time.Time `json:"created_time" sql:"not null;type:date"`
+	updated_time         time.Time `json:"updated_time" sql:"not null;type:date"`
+}
---
+++ mysql/db/init.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS user_db;
+CREATE DATABASE user_db;
+USE user_db;
+
+CREATE TABLE users (
+  id integer not null auto_increment,
+  name text,
+  accepted_count integer,
+  accepted_count_rank integer,
+  rated_point_sum integer,
+  rated_point_sum_rank integer,
+  created_time datetime default current_timestamp,
+  updated_time datetime not null default current_timestamp on update current_timestamp,
+  primary key (id)
+)DEFAULT CHARACTER SET=utf8;
+
+INSERT INTO users (name) VALUES ("太郎"),("花子"),("令和");
